c1f35910a1a7c01828d93d6a0fcf9346f25c4f54
Karenw1004 <karenw0103@gmail.com>
2021-01-21 23:11:49 +0700

    UI join meeting button has been updated with a spinner QPushButton

--- app.py
+++ app.py
@@ -1,10 +1,51 @@
 from PyQt5 import uic
 import sys
 from host import ZoomBackend
-from PyQt5.QtCore import pyqtSignal, QCoreApplication
-from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication, QMessageBox, QDialog
+from PyQt5.QtCore import pyqtSignal, QCoreApplication,QObject, QThread
+from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication, QMessageBox, QDialog, QPushButton
 import threading
+from waitingspinnerwidget import QtWaitingSpinner
+from PyQt5 import QtCore, QtGui, QtWidgets
 
+
+# Step 1: Create a worker class
+class Worker(QThread):
+
+    finished = pyqtSignal()
+    def __init__(self, id, pwd, url, parent=None):
+        QThread.__init__(self, parent)
+        self.meetingId = id
+        self.password = pwd
+        self.url = url
+
+    def run(self):
+        linkFunc(self.meetingId, self.password, self.url)
+        self.finished.emit()
+
+class LoadingButton(QPushButton):
+    # @QtCore.pyqtSlot()
+    def start(self):
+        if hasattr(self, "_movie"):
+            self._movie.start()
+
+    # @QtCore.pyqtSlot()
+    def stop(self):
+        if hasattr(self, "_movie"):
+            self._movie.stop()
+            self.setIcon(QtGui.QIcon())
+
+    def setGif(self, filename):
+        if not hasattr(self, "_movie"):
+            self._movie = QtGui.QMovie(self)
+            self._movie.setFileName(filename)
+            self._movie.frameChanged.connect(self.on_frameChanged)
+            if self._movie.loopCount() != -1:
+                self._movie.finished.connect(self.start)
+        self.stop()
+
+    @QtCore.pyqtSlot(int)
+    def on_frameChanged(self, frameNumber):
+        self.setIcon(QtGui.QIcon(self._movie.currentPixmap()))
 
 class loginWindow(QDialog):
     def __init__(self):
@@ -14,7 +55,17 @@ class loginWindow(QDialog):
         self.password = None
         self.url = None
         self.mainWindow = MainWindow()
-        self.login.pushButton_login.clicked.connect(self.openMainWindow)
+
+        # self.pushButton_login = QPushButton("Join Meeting") 
+        # self.spinner = QtWaitingSpinner(self, disableParentWhenSpinning=True)
+        # self.login.gridLayout.addWidget(self.pushButton_login)
+        # self.login.gridLayout.addWidget(self.spinner)
+
+        self.pushButton_login = LoadingButton("Join Meeting") 
+        self.pushButton_login.setGif("./UI/images/loading.gif")
+
+        self.login.gridLayout.addWidget(self.pushButton_login)
+        self.pushButton_login.clicked.connect(self.runLinkFunc)
 
     def getInfo(self):
         self.meetingId = self.login.lineEdit_meeting_id.text()
@@ -24,15 +75,34 @@ class loginWindow(QDialog):
         print(f"password: {self.password}")
         print(f"url: {self.url}")
 
-    def openMainWindow(self):
+    def runLinkFunc(self):
+        self.pushButton_login.start()
+
+        # self.spinner.start()
+        # self.pushButton_login.setText("Joining meeting..")
         self.getInfo()
-        linkFunc(self.meetingId, self.password, self.url)
-        self.mainWindow.main.show()
+
+        self.thread = QThread()
+        self.worker = Worker(self.meetingId, self.password, self.url)
+        self.worker.moveToThread(self.thread)
+        self.thread.started.connect(self.worker.run)
+        self.worker.finished.connect(self.thread.quit)
+        self.worker.finished.connect(self.worker.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
+        self.thread.start()
+
+        # Final resets
+        self.pushButton_login.setEnabled(False)
+        self.thread.finished.connect(lambda: self.openMainWindow())
+        
         print("after open main")
         # web_thread = threading.Thread(target=linkFunc(self.meetingId, self.password, self.url))
         # web_thread.start()
         # web_thread.join()
 
+    def openMainWindow(self):
+        self.mainWindow.main.show()
+        self.login.hide()
 
 class MainWindow(QMainWindow):
     def __init__(self):
