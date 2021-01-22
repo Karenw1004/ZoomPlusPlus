a93375d15eafb6300b40466232c3a059c7073562
shulala <zj2060203064@gmail.com>
2021-01-22 17:31:15 -0600

    added 2 tables which shows students' attention and  students who raised hand

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from PyQt5 import uic
 import sys
 from host import ZoomBackend
 from PyQt5.QtCore import pyqtSignal, QCoreApplication
-from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication, QMessageBox, QDialog
+from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication, QMessageBox, QDialog, QTabWidget, QTableWidgetItem, QHeaderView
 import threading
 
 
@@ -28,7 +28,9 @@ class loginWindow(QDialog):
         self.getInfo()
         self.mainWindow.main.show()
         print("after open main")
-        linkFunc(self.meetingId, self.password, self.url)
+        self.mainWindow.createHandsTable(10, 1, [])
+        self.mainWindow.createAttentionTable(15, 2, [])
+        # linkFunc(self.meetingId, self.password, self.url)
         # web_thread = threading.Thread(target=linkFunc(self.meetingId, self.password, self.url))
         # web_thread.start()
         # web_thread.join()
@@ -38,7 +40,32 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.main = uic.loadUi("./UI/main_window.ui")
-        # self.main.pushButton_test.clicked.connect(lambda: print("testing"))
+        self.table_student_attention = self.main.tableWidget_attention
+        self.table_hands = self.main.tableWidget_hands
+        self.main.pushButton_test.clicked.connect(lambda: print("testing"))
+
+    def createAttentionTable(self, row, col, lst):
+        # set row which is the number of students
+        self.table_student_attention.setRowCount(row)
+        # set col should be 2
+        self.table_student_attention.setColumnCount(col)
+        # set header
+        self.table_student_attention.setHorizontalHeaderLabels(['Name', 'Attention(%)'])
+        # set everything in the lst, but I don't have list right now
+        for i in range(row):
+            self.table_student_attention.setItem(i, 0, QTableWidgetItem("cell"+str(i)))
+            self.table_student_attention.setItem(i, 1, QTableWidgetItem("cell"+str(i+1)))
+        self.table_student_attention.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+
+    def createHandsTable(self, row, col, lst):
+        # set row
+        self.table_hands.setRowCount(row)
+        # set col should be 1
+        self.table_hands.setColumnCount(col)
+        self.table_hands.setHorizontalHeaderLabels(["Raised hand students"])
+        for i in range(row):
+            self.table_hands.setItem(i, 0, QTableWidgetItem("student"+str(i)))
+        self.table_hands.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
 
 def linkFunc(id, pwd, url):
